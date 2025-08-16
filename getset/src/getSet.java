506ac8a87df233bdfec3e87a736f3ccca94380cc
Leticia Suelen Carvalho Germano <diogogermano28@gmail.com>
2025-08-16 12:21:58 -0300

    remove

--- getset/src/getSet.java
+++
@@ -1,9 +0,0 @@
-public class getSet {
-    public static void main (String[] args) {
-        Caneta c1 = new Caneta ("BIC", "Vermelha", 0.7f);
-        c1.status();
-        System.out.println("\n");
-        Caneta c2 = new Caneta("Faber", "Rosa", 0.1f);
-        c2.status();
-    }
-}
